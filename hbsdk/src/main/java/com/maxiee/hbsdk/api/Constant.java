5efc0778f0de052bc34a6c10ee6ae53279549e36
maxiee <maxieewong@gmail.com>
2016-01-14 00:15:48 +0800

    Constant.java: URIs for ContentProvider.

---
+++ hbsdk/src/main/java/com/maxiee/hbsdk/api/Constant.java
@@ -0,0 +1,12 @@
+package com.maxiee.hbsdk.api;
+
+/**
+ * Created by maxiee on 16/1/10.
+ */
+public class Constant {
+
+    public static final String BASE_URI = "content://com.maxiee.heartbeat.provider/";
+    public static final String CATEGORY_EVNET = "event/";
+
+    public static final String API_EVENT_RANDOM = BASE_URI + CATEGORY_EVNET + "random";
+}

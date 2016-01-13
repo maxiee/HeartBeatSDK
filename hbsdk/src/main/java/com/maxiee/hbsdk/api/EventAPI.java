bf3a1f58ddf491d979c065a49827b74356cf19c1
maxiee <maxieewong@gmail.com>
2016-01-14 00:19:13 +0800

    EventAPI.java: APIs related to events.

---
+++ hbsdk/src/main/java/com/maxiee/hbsdk/api/EventAPI.java
@@ -0,0 +1,26 @@
+package com.maxiee.hbsdk.api;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.maxiee.hbsdk.database.utils.EventUtils;
+import com.maxiee.hbsdk.model.Event;
+
+/**
+ * Created by maxiee on 16/1/10.
+ */
+public class EventAPI {
+
+    public static Event random(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        Uri uri = Uri.parse(Constant.API_EVENT_RANDOM);
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        if (cursor == null || cursor.getCount() < 1) return null;
+        cursor.moveToNext();
+        Event e = EventUtils.queryEvent(cursor);
+        cursor.close();
+        return e;
+    }
+}

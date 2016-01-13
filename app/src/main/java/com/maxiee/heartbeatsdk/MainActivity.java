ee0ceb014efead3bf3b2d721fce14a3a53985acf
maxiee <maxieewong@gmail.com>
2016-01-14 00:13:11 +0800

    MainActivity.java: random event button.

--- app/src/main/java/com/maxiee/heartbeatsdk/MainActivity.java
+++ app/src/main/java/com/maxiee/heartbeatsdk/MainActivity.java
@@ -2,12 +2,30 @@ package com.maxiee.heartbeatsdk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.maxiee.hbsdk.api.EventAPI;
+import com.maxiee.hbsdk.model.Event;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.random_event)    Button  mRandomEventButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.random_event)
+    public void randomEvent() {
+        Event e = EventAPI.random(this);
+        Toast.makeText(MainActivity.this, e.getEvent(), Toast.LENGTH_SHORT).show();
     }
 }

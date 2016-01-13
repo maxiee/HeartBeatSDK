0c956f79e521a90034c6466679f3ced58ee0fe18
maxiee <maxieewong@gmail.com>
2016-01-14 00:18:17 +0800

    Event.java: event model.

---
+++ hbsdk/src/main/java/com/maxiee/hbsdk/model/Event.java
@@ -0,0 +1,45 @@
+package com.maxiee.hbsdk.model;
+
+/**
+ * Created by maxiee on 15-6-12.
+ */
+public class Event {
+    private String mEvent;
+    private long timestamp;
+    private long mId;
+
+    public Event(
+            int id,
+            String mEvent,
+            long timestamp) {
+        this.mId = id;
+        this.mEvent = mEvent;
+        this.timestamp = timestamp;
+    }
+
+    public Event(
+            long id,
+            String mEvent,
+            long timestamp) {
+        this.mId = (int) id;
+        this.mEvent = mEvent;
+        this.timestamp = timestamp;
+    }
+
+    public String getEvent() {
+        return mEvent;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public long getId() {
+        return mId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d][%s]\n", mId, mEvent);
+    }
+}
